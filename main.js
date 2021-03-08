671f1643b0f822dfa83a855a84755ccbec577748
Muhammad47 <ranbo470@gmail.com>
2021-03-08 21:38:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function funcard1() {
+    var x = document.getElementById("card1p");
+    if (x.style.display === "none") {
+      x.style.display = "inline";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function funcard2() {
+    var x = document.getElementById("card2p");
+    if (x.style.display === "none") {
+      x.style.display = "inline";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function funcard3() {
+    var x = document.getElementById("card3p");
+    if (x.style.display === "none") {
+      x.style.display = "inline";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
